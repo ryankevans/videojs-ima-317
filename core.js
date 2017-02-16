406e6cf5ee85a1bff3ac936a2442d1a20fbdecc9
Ryan K. Evans <ryankevans@server.fake>
2017-02-15 19:46:41 -0800

    jQuery + documentation

--- core.js
+++ core.js
@@ -1,18 +1,17 @@
 var config = {
+	playerId: 'content_video',
     playerWidth: 640,
     playerHeight: 360,
 
-    thumbnailUrl: 'https://googleads.github.io/videojs-ima/examples/posters/android.png',
-    sources: [
-      {
-        type :'video/mp4',
-        src: 'http://rmcdn.2mdn.net/Demo/vast_inspector/android.mp4'
-      }
-    ],
+    thumbnailUrl: '//googleads.github.io/videojs-ima/examples/posters/android.png',
+    sources: [{
+	    type :'video/mp4',
+	    src: 'http://rmcdn.2mdn.net/Demo/vast_inspector/android.mp4'
+    }],
 
     // LKQD Offical Demo Tag: Desktop+Mobile In-Banner
     // -> http://demo.lkqd.com/formats.html
-    adTagUrl: 'http://v.lkqd.net/ad?pid=21&sid=71907&formats=true&output=vastvpaid&support=html5&execution=inbanner&placement=&playinit=auto&volume=100&width=$$width$$&height=$$height$$&pageurl=$$url$$&rnd=$$random$$&m='
+    adTagUrl: '//v.lkqd.net/ad?pid=21&sid=71907&formats=true&output=vastvpaid&support=html5&execution=inbanner&placement=&playinit=auto&volume=100&width=$$width$$&height=$$height$$&pageurl=$$url$$&rnd=$$random$$&m='
 };
 
 function regexEscape(s) {
@@ -31,42 +30,39 @@ function populateAdTagMacros(url) {
 	    'random': Math.floor(Math.random() * 100000000),
 	    'url': 'http://demo.lkqd.com'
 	};
-
-    Object.keys(macros).forEach(function(k) {
-    	url = strReplaceAll(url, '$$' + k + '$$', macros[k]);
+	$.each(macros, function(k, v) {
+    	url = strReplaceAll(url, '$$' + k + '$$', v);
     });
     return url;
 }
 
 function createPlayer() {
-	var dumbPlayer = document.createElement('video');
-	dumbPlayer.id = 'content_video';
-	dumbPlayer.className = 'video-js vjs-default-skin vjs-big-play-centered';
-	dumbPlayer.setAttribute('width', config.playerWidth + 'px');
-	dumbPlayer.setAttribute('height', config.playerHeight + 'px');
-	var contentSrc = document.createElement('source');
-	contentSrc.setAttribute('src', config.sources[0].src);
-	contentSrc.setAttribute('type', config.sources[0].type);
-	dumbPlayer.appendChild(contentSrc);
-	var placeholder = document.getElementById('ima-sample-placeholder');
-	placeholder.parentNode.appendChild(dumbPlayer);
-	placeholder.parentNode.removeChild(placeholder);
-	return 'content_video';
+	var dumbPlayer = $('<video>', {
+		id: config.playerId,
+		class: 'video-js vjs-default-skin vjs-big-play-centered',
+		width: config.playerWidth + 'px',
+		height: config.playerHeight + 'px'
+	});
+	var contentSrc = $('<source>', {
+		src: config.sources[0].src,
+		type: config.sources[0].type
+	});
+	dumbPlayer.append(contentSrc);
+	$('#ima-sample-placeholder').after(dumbPlayer).remove();
 }
 
 function initPlayer(options) {
-	var playerId = createPlayer();
-
+	createPlayer();
 	var playerOptions = {
         preload: options.preload,
         controls: true,
         poster: config.thumbnailUrl
 	};
 
-	var player = videojs(playerId, playerOptions);
+	var player = videojs(config.playerId, playerOptions);
 	player.ima({id: player.id()}, function() { player.ima.startFromReadyCallback(); });
 
-	player.one('touchstart', function () {
+	player.one('click', function () {
 		if (options.start) { options.start(player); }
 		player.ima.requestAds();
 		player.ima.initializeAdDisplayContainer();
